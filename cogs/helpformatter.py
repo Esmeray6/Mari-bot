0dff928c94f9144457a282e6eee659be275bdeec
shivaco <ezboost322@gmail.com>
2018-07-13 22:57:41 +0600

    Fix help command breaking itself when command help text wasn't specified.

--- cogs/helpformatter.py
+++ cogs/helpformatter.py
@@ -17,14 +17,14 @@ class Help:
             for smth in li:
                 if smth != 'Help' and smth != 'OwnerCog' and smth != 'Status':
                     s = list(self.bot.get_cog_commands(smth))
-                    result += '\n' + s[0].cog_name + ':\n' + '    '.join('\n    {} - {}\n'.format(c.name, c.help) for c in s)
+                    result += '\n' + s[0].cog_name + ':\n' + '    '.join('\n    {} - {}\n'.format(c.name, str(c.help)) for c in s)
             await ctx.send(pref + result + postf)
         else:
             if command not in self.bot.all_commands:
                 if command not in self.bot.cogs:
                     cmd = self.bot.get_command(command)
                     if cmd:
-                        result += cmd.signature + '\n\n    ' + cmd.help
+                        result += cmd.signature + '\n\n    ' + str(cmd.help)
                         await ctx.send(pref + result + postfix)
                     else:
                         result = 'That command/category/command group does not exist!'
@@ -33,14 +33,12 @@ class Help:
                     the_cog = list(self.bot.get_cog_commands(command))
                     result += the_cog[0].cog_name + ':\n'
                     for cmd in the_cog:
-                        result += ''.join('\n    {} - {}\n'.format(cmd.name, cmd.help))
+                        result += ''.join('\n    {} - {}\n'.format(cmd.name, str(cmd.help)))
                     await ctx.send(pref + result + postfix)
 
             else:
                 cmd = self.bot.get_command(command)
-                # helptext = ' '.join('[{}]'.format(cmd) for (cmd, param) in dict(cmd.clean_params).items())
-                # result += '' + cmd.name + ' ' + helptext + '\n\n    ' + cmd.help + '\n'
-                result += cmd.signature + '\n\n    ' + cmd.help
+                result += cmd.signature + '\n\n    ' + str(cmd.help)
                 await ctx.send(pref + result + postfix)
 
 def setup(bot):
