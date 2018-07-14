534d866f89f3a5697429312f676cd53064da0ce9
shivaco <ezboost322@gmail.com>
2018-07-14 21:08:48 +0600

    Added eval command called py.

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,11 +1,96 @@
 import discord
 from discord.ext import commands
 import datetime
+import time
+import textwrap
+import re
+from io import StringIO
+import io
+import traceback
+from contextlib import redirect_stdout
+from enum import Enum
+from random import randint, choice
+from urllib.parse import quote_plus
+import aiohttp
 
 class OwnerCog:
 
     def __init__(self, bot):
         self.bot = bot
+
+    def cleanup_code(self, content):
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+
+        # remove `foo`
+        return content.strip('` \n')
+
+    async def interpreter(self, env, code, ctx):
+        body = self.cleanup_code(code)
+        stdout = io.StringIO()
+
+        to_compile = 'async def func():\n{}'.format(textwrap.indent(body, "  "))
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send('```\n{}: {}\n```'.format(e.__class__.__name__, e))
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send('```\n{}{}\n```'.format(value, traceback.format_exc()))
+        else:
+            value = stdout.getvalue()
+
+            result = None
+            if ret is None:
+                if value:
+                    result = '```\n{}\n```'.format(value)
+                else:
+                    try:
+                        result = '```\n{}\n```'.format(repr(eval(body, env)))
+                    except:
+                        pass
+            else:
+                self._last_result = ret
+                result = '```\n{}{}\n```'.format(value, ret)
+
+            if result:
+                if len(str(result)) > 1950:
+                    await ctx.send(result[0:1950])
+                    await ctx.send(result[1951:-1]) # Feel free to fix this. I am lazy.
+
+                else:
+                    await ctx.send(result)
+
+    @commands.command()
+    @commands.is_owner()
+    async def py(self, ctx, *, msg):
+        """Python interpreter. See the wiki for more info."""
+
+        if ctx.invoked_subcommand is None:
+            env = {
+                'bot': self.bot,
+                'self': self,
+                'client': self.bot,
+                'ctx': ctx,
+                'channel': ctx.channel,
+                'author': ctx.author,
+                'guild': ctx.guild,
+                'server': ctx.guild,
+                'message': ctx.message,
+                'msg': ctx.message
+            }
+
+            env.update(globals())
+
+            await self.interpreter(env, msg, ctx)
 
     @commands.command()
     @commands.is_owner()
