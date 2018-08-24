2acceda0926029beee7efae004532ee5b1fe5afc
shivaco <ezboost322@gmail.com>
2018-08-25 00:59:34 +0600

    Show cog name when help is used on a command.

--- cogs/helpformatter.py
+++ cogs/helpformatter.py
@@ -58,7 +58,7 @@ class Help:
                         await ctx.send(page)
             else:
                 cmd = self.bot.get_command(command.replace('*', '').replace(self.bot.user.mention, ''))
-                result += f"{ctx.prefix.replace(self.bot.user.mention, f'@{self.bot.user.name}#{self.bot.user.discriminator} ')}{cmd.signature}\n\n    {cmd.help}"
+                result += f"{ctx.prefix.replace(self.bot.user.mention, f'@{self.bot.user.name}#{self.bot.user.discriminator} ')}{cmd.signature}\n\nCog: {cmd.cog_name}\n\n    {cmd.help}"
                 await ctx.send(pref + result + postfix)
 
 def setup(bot):
